13aad34377092b9ed1d54d7f19e67ef91b5baa6a
joe-practice <junk1@nycmail.com>
2017-03-01 14:59:53 -0800

    Update github_watchdog.py

--- github_watchdog.py
+++ github_watchdog.py
@@ -3,6 +3,7 @@
 
 import shelve
 import configparser
+import logging
 from slacker import Slacker
 from github import Github
 
@@ -24,7 +25,15 @@ class Contrib():
         slack=Slacker(self.slack_token)
         slack.chat.post_message(self.slack_channel,'alert: new contributor detected')
 
+    def log(self,message):
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(levelname)s %(message)s',
+                            filename='github_watchdog.log',
+                            filemode='a')
+        logging.info(message)
+
     def count_contribs(self):
+        contrib_arr=[]
         count=0
         g=Github()
         r=g.get_repo(self.repo)
@@ -32,21 +41,27 @@ class Contrib():
         for each in contrib_list:
             count+=1
             print each.login
-        return count
+            contrib_arr.append(each.login)
+        new_contrib=contrib_arr[-1]
+        return count,new_contrib
 
     def check_contribs(self):
+        current,new_contrib=self.count_contribs()
         d = shelve.open('github_watchdog')
         flag = 'contrib_count' in d
         if flag:
-            if (d['contrib_count']) == self.count_contribs():
-                self.slack_alert()
+            if (d['contrib_count']) == current:
+                #self.slack_alert()
+                self.log('no change in contributors')
                 print('no change in number of contribs')
             else:
-                self.slack_alert()
+                #self.slack_alert()
+                self.log('new contributor: '+new_contrib)
+                d['contrib_count']=current
                 print('ermagerd, more contribs!!!')
         else:
             print('initializing state')
-            d['contrib_count']=self.count_contribs()
+            d['contrib_count']=current
         d.close()
 
 c=Contrib()
