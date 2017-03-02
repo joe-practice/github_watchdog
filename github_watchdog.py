9489fdca931d0361fb13d949c44dc5567133fa06
joe-practice <junk1@nycmail.com>
2017-03-01 23:14:58 -0800

    Update github_watchdog.py

--- github_watchdog.py
+++ github_watchdog.py
@@ -41,7 +41,7 @@ class Contrib():
 
     def check_contribs(self):
         current, new_contrib = self.count_contribs()
-        d = shelve.open('./persist/github_watchdog')
+        d = shelve.open('/github_watchdog/persist/gw_shelve')
         flag = 'contrib_count' in d
         if flag:
             if (d['contrib_count']) == current:
